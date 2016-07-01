2a0e42ca960365e5016786a95fcc3f9d88b1a7c1
Nathan Erickson <nerickson@906technologies.com>
2016-07-01 13:55:02 -0400

    Updated for docker-engine 1.11.0> , older version missing full support for token authentication.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
-
+				
 require_once __DIR__.'/vendor/autoload.php';
 
 try {
@@ -16,51 +16,123 @@ try {
 } catch (ParseException $e) {
     die('Unable to parse config.yml');
 }
-
 $app = new Application();
 
-$privateKeyContents = file_get_contents($config['private_key']);
-$privateKey = openssl_pkey_get_private($privateKeyContents);
-
-$kid = getKid($privateKey);
-
-function getKid($privateKey){
-	$details = openssl_pkey_get_details($privateKey);
-	
-	$publicKey = str_replace(['-----BEGIN PUBLIC KEY-----', '-----END PUBLIC KEY-----'], '', $details['key']);
-	$publicKey = trim($publicKey);
-	$der = base64_decode($publicKey);
-	
-	$hashDer = hash('sha256', $der, true);
-	
-	$first30bytes = substr($hashDer, 0, 30);
-	
-	$libtrust = Base32\Base32::encode($first30bytes);
-	
-	$chunks = [];
-
-	for ($i = 0, $max = 48; $i < $max; $i += 4) {
-		$chunk = substr($libtrust, $i, 4);
-		$chunks[] = $chunk;
-	}
-	
-	$fingerprint = implode(":", $chunks);
-	
-	return $fingerprint;
-}
+$privateKey = openssl_pkey_get_private(file_get_contents($config['private_key']));
 
 $log = new Logger('name');
 $log->pushHandler(new StreamHandler('auth.log', Logger::INFO));
 
+class TokenGenerator {
+	private $user;
+	private $issuer;
+	private $audience;
+	private $privateKey;
+	private $kid;
+	
+	
+	public function __construct($issuer, $audience, $privateKey) {
+		$this->issuer = $issuer;
+		$this->audience = $audience;
+		$this->privateKey = $privateKey;
+		$this->kid = $this->getKid($this->privateKey);
+	}
+	
+	public function setUser($user){
+		$this->user = $user;
+	}
+	
+	public function createAccessToken($scope, $expires){
+		$token = array(
+			"iss" => $this->issuer,
+			"aud" => $this->audience,
+			"iat" => time(), // (Issued At)
+			"nbf" => time(), // (Not Before)
+			"sub" => $this->user,
+			"jti" => uniqid() // (JWT ID)
+		);
+		$token["exp"] = $expires;
+
+		$token['access'] = [];
+
+		$token['access'][] = [
+			'type' => 'registry',
+			'name' => 'catalog',
+			'actions' => ['*']
+		];
+
+		if($scope){
+			list($type, $name, $actions) = explode(":", $scope, 3);
+			$actions = explode(",", $actions);
+
+			$token['access'][] = [
+				'type' => $type,
+				'name' => $name,
+				'actions' => $actions
+			];
+		}
+		return JWT::encode($token, $this->privateKey, 'RS256', $this->kid);
+	}
+	
+	public function createRefreshToken(){
+		$token = array(
+			"iss" => $this->issuer,
+			"aud" => $this->audience,
+			"iat" => time(), // (Issued At)
+			"nbf" => time(), // (Not Before)
+			"sub" => $this->user,
+			"jti" => uniqid() // (JWT ID)
+		);
+		return JWT::encode($token, $this->privateKey, 'RS256', $this->kid);
+	}
+	
+	private function getKid($privateKey){
+		$details = openssl_pkey_get_details($privateKey);
+
+		$publicKey = str_replace(['-----BEGIN PUBLIC KEY-----', '-----END PUBLIC KEY-----'], '', $details['key']);
+		$publicKey = trim($publicKey);
+		$der = base64_decode($publicKey);
+
+		$hashDer = hash('sha256', $der, true);
+
+		$first30bytes = substr($hashDer, 0, 30);
+
+		$libtrust = Base32\Base32::encode($first30bytes);
+
+		$chunks = [];
+
+		for ($i = 0, $max = 48; $i < $max; $i += 4) {
+			$chunk = substr($libtrust, $i, 4);
+			$chunks[] = $chunk;
+		}
+
+		$fingerprint = implode(":", $chunks);
+
+		return $fingerprint;
+	}
+	
+	function verifyRefreshToken($token){
+		try{
+			$details = openssl_pkey_get_details($this->privateKey);
+			$publicKey = openssl_pkey_get_public($details['key']);
+			$payload = JWT::decode($token, $publicKey, array('RS256'));
+		} catch (\Firebase\JWT\ExpiredException $e){
+			return false;
+		} catch (\Exception $e){
+			return false;
+		}
+		return $payload;
+	}
+}
+
 //$app['debug'] = true;
 
-function pamAuthenticate($user, $password){
+function pamAuthenticate($user, $password, $blacklistedUsers = array()){
 	if (!$user || !$password || strlen($user) > 30 || strlen($password) > 60){
 		throw new \Exception ('Missing Creds');
 	}
 
 	$error = '';
-	$blacklistedUsers = array('elegantseagulls'); //k
 
 	if (in_array($user, $blacklistedUsers) || !pam_auth($user, $password, $error, false)) {
 		throw new \Exception ('Pam Authenticaiton Failed : '.$error);
@@ -71,110 +143,124 @@ function pamAuthenticate($user, $password){
 $app->get('/', function (Request $request) {
 	return  "<h1>This is not the auth you are looking for</h1>";
 });
-	
-$app->get('/docker/v2/token', function (Request $request) use ($app, $privateKey, $log, $kid, $config) {
+
+
+$app->get('/docker/v2/token', function (Request $request) use ($app, $privateKey, $log, $config) {
 	JWT::$leeway = 60; // $leeway in seconds
 	$log->info('Requested', [$request->getUri()]);
+	
+	$log->info('Headers', $request->headers->all());
 	
 	$authorizationHeader = $request->headers->get('authorization');
 	
 	if(!$authorizationHeader){
 		$log->error('Missing authorization Header', []);
-		return$app->json(
+		return $app->json(
 			["errors" => [
 				"code" => "UNAUTHORIZED"
 				]
 			], 401);
 	}
 	
-	if (strpos($authorizationHeader, "Basic") === 0){
-		$log->info('Token Requested', []);
-		$log->info('Params', $request->query->all());
+	if (strpos($authorizationHeader, "Basic") !== 0){
+		$log->error('Missing Basic auth Header', []);
+		return $app->json(
+			["errors" => [
+				"code" => "UNAUTHORIZED"
+				]
+			], 401);
+	}
+	
+
+	$log->info('Params', $request->query->all());
+
+	$credentialsEncoded = str_replace('Basic ', '', $authorizationHeader);
+	list($user, $password) = explode(':', base64_decode($credentialsEncoded), 2);
+
+	$log->info('User', ["username"=>$user]);
+
+	try{
+		if(!isset($config["blacklisted_users"])){
+			$config["blacklisted_users"] = array();
+		}
 		
-		$credentialsEncoded = str_replace('Basic ', '', $authorizationHeader);
-		list($user, $password) = explode(':', base64_decode($credentialsEncoded), 2);
-		
-		$log->info('User', ["username"=>$user]);
-		
-		try{
-			pamAuthenticate($user, $password);
-			
-			$expires = strtotime('+3 day', time());
-			$token = array(
-				"iss" => $config["issuer"],
-				"aud" => $config["audience"],
-				"iat" => time(),
-				"nbf" => time(),
-				"exp" => $expires,
-				"sub" => $user,
-				"jti" => uniqid(),
-				"user" => $user
-			);
-			$scope = $request->query->get('scope');
-			
-			if($scope){
-				list($type, $name, $actions) = explode(":", $scope, 3);
-				$actions = explode(",", $actions);
-				
-				$token['access'] = [[
-					'type' => $type,
-					'name' => $name,
-					'actions' => $actions
-				]];
-			}
-			
+		pamAuthenticate($user, $password, $config["blacklisted_users"]);
+
+		$tokenGenerator = new TokenGenerator($config["issuer"], $config["audience"], $privateKey);
+		$tokenGenerator->setUser($user);
+
+
+		if($request->query->get("offline_token") == true){
+			$log->info('Offline Token Requested',[]);
 			$now = new \DateTime();
+			$expires = strtotime('+3 day', time());
 			
-			$log->info('Response Claims Created', $token);
-			
-			$response = [
-				'token' => JWT::encode($token, $privateKey, 'RS256', $kid),
+			return $app->json([
+				'access_token' => $tokenGenerator->createAccessToken($request->query->get('scope'), $expires),
+				'refresh_token' => $tokenGenerator->createRefreshToken(),
 				'expires_in' => ($expires - time()),
-				'issued_at' => $now->format(\DateTime::RFC3339)];
-		
-			
-			//$log->info('Authentication Response', $response);
-			
-			return $app->json($response, 200, array());
-				
-		} catch (\Exception $e){
-			$log->error('Validation Error: '.$e->getMessage(), []);
-			return $app->json('Invalid Credentials', 403);
-		}
-		
-	} else if (strpos($authorizationHeader, "Bearer") === 0){ 
-		//this will never get hit because docker has the public key and can varify the validity of the token itself
-		//none the less its still good to have for testing if tokens generated can be verified by the public key
-		
-		$log->info('TOKEN VERIFICATION', []);
-		
-		$token = str_replace('Bearer ', '', $authorizationHeader);
-		
-		try{
-			$details = openssl_pkey_get_details($privateKey);
-			$publicKey = openssl_pkey_get_public($details['key']);
-			$payload = JWT::decode($token, $publicKey, array('RS256'));
-		} catch (\Firebase\JWT\ExpiredException $e){
-			$log->error('Token Expired', []);
-			return $app->json('Invalid Token', 403);
-		} catch (\Exception $e){
-			$log->error('Token Verification Error: '.$e->getMessage(), []);
-			return $app->json('Invalid Token', 403);
-		}
-		
-		return $app->json([], 200, array());
+				'issued_at' => $now->format(\DateTime::RFC3339)
+			], 200, array());
+		} 
+
+		$log->error('offline token request missing, docker version of requester might be out of date', []);
+		return $app->json(
+			["errors" => [
+				"code" => "UNAUTHORIZED"
+				]
+			], 401);
+
+
+	} catch (\Exception $e){
+		$log->error('Validation Error: '.$e->getMessage(), []);
+		return $app->json('Invalid Credentials', 403);
 	}
 
-
-	
-	$log->error("End point hit however no auth request or verification was made.",[]);
-	
-	return $app->json(
-		["errors" => [
-			"code" => "UNAUTHORIZED"
-		]
-		], 401);
 });
+
+$app->post('/docker/v2/token', function (Request $request) use ($app, $privateKey, $log, $config) {
+	$log->info('TOKEN VERIFICATION REQUESTED', [$request->getUri()]);
+	
+	$log->info('REQUEST', $request->request->all());
+	
+	$tokenGenerator = new TokenGenerator($config["issuer"], $config["audience"], $privateKey);
+	
+	$refreshToken = $request->request->get('refresh_token');
+	$refreshTokenPayload = $tokenGenerator->verifyRefreshToken($refreshToken);
+	
+	$log->info('TOKEN PAYLOAD', [$refreshTokenPayload]);
+	
+	if(!$refreshTokenPayload){
+		$log->error('Token Expired', []);
+		return $app->json('Invalid Token', 403);
+	}
+	
+	$user = $refreshTokenPayload->sub;
+	
+	$log->info('USER', [$user]);
+	
+	$scope = $request->request->get('scope');
+	
+	$log->info('SCOPE', [$scope]);
+	
+	$tokenGenerator->setUser($user);
+	
+	
+	$now = new \DateTime();
+	$expires = strtotime('+3 day', time());
+	
+	$response = [
+		'access_token' => $tokenGenerator->createAccessToken($request->request->get('scope'), $expires),
+		'refresh_token' => $tokenGenerator->createRefreshToken(),
+		'expires_in' => ($expires - time()),
+		'issued_at' => $now->format(\DateTime::RFC3339)
+	];
+	
+	$log->info('RESPONSE', $response);
+
+	return $app->json($response, 200);	
+});
+
 
 $app->run();
 
